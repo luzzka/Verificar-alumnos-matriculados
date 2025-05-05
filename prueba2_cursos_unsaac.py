3d17ff1122d3c02a3d4a672905e2b6bee7dac6f8
vane <vmreynagaflores@gmail.com>
2025-05-04 22:59:08 -0500

    Update functions file

--- prueba2_cursos_unsaac.py
+++ prueba2_cursos_unsaac.py
@@ -1,82 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-
-# =============================================================================================================================================
-def get_course_info(soup_text):
-    """Extracts course information from the HTML soup."""
-    # extract the 1st table
-    soup = soup_text
-    tablas = soup.find_all("table", class_="ttexto")
-    tabla_info = tablas[-2]  
-    
-    info = {}
-    filas_info = tabla_info.find_all("tr")
-    for fila in filas_info:
-        celdas = fila.find_all("td")
-        for i in range(0, len(celdas), 2):
-            clave = celdas[i].text.strip().replace(":", "")
-            valor = celdas[i+1].text.strip()
-            info[clave] = valor
-    return info
-
-# =============================================================================================================================================
-def get_student_info(soup_text):
-    """Extracts name and codes of students from the HTML soup."""
-    # extract the 2nd table
-    soup = soup_text
-    tablas = soup.find_all("table", class_="ttexto")
-    tabla_estudiantes = tablas[-1]
-    
-    filas = tabla_estudiantes.find_all("tr")
-    data = []
-    for fila in filas[1:]:
-        celdas = fila.find_all("td")
-        if len(celdas) >= 3:
-            codigo = celdas[1].text.strip()
-            nombre = celdas[2].text.strip()
-            data.append([codigo, nombre])
-    df = pd.DataFrame(data, columns=["Código", "Nombre"])
-    return df
-
-# =============================================================================================================================================
-def get_students_for_course(course_code, semester="2025-1"): 
-    """Extracts information of teachers and students from a given course."""
-    url = "http://ccomputo.unsaac.edu.pe/index.php?op=alcurso"
-    data = {
-        "curso": course_code.upper(),  # Course code field name. Using upper() to ensure uppercase.
-        "semestre": semester,       # Semester field name.
-        "Consultar": "Consultar"   # Submit button name and value.
-    }
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"  # Add user agent
-    }
-
-    response = requests.post(url, data=data, headers=headers)
-
-    if response.status_code == 200:
-        # Check if the course is not found.
-        if "No existe curso" in response.text:
-            print("Course not found")
-            return None 
-        
-        soup = BeautifulSoup(response.content, "html.parser")
-
-        info_curso = get_course_info(soup)
-        print("Información del curso:")
-        for clave, valor in info_curso.items():
-            print(f"  {clave}: {valor}")
-
-        info_estudiantes = get_student_info(soup)
-        print(info_estudiantes.head())
-        
-    else:
-        print(f"Error: {response.status_code}")
-        return None
+import functions as func
 
 # Example usage
-course_code = "IF616AIN"
+course_code = "IF662AIN"
 course_codes = [
     'IF619AIN', 'IF061AIN', 'IF662AIN', 'IF616AIN', 'IF484AIN', 'ME359ZIN', 'ME358ZIN', 'IF553AIN', 'IF653AIN', 
     'IF554AIN', 'IF556AIN', 'IF618ZIN', 'MEG01AIN', 'MEG01BIN', 'MEG02AIN', 'MEG02BIN', 'CBG01AIN', 'CBG01BIN', 
@@ -91,7 +16,8 @@ course_codes = [
     'IF466AIN', 'IF613AIN', 'IF613BIN', 'IF651AIN', 'IF651BIN', 'IF063AIN', 'IF552AIN', 'IF552BIN', 'IF652AIN', 
     'IF664AIN', 'IF664BIN', 'IF614AIN', 'IF669AIN', 'IF482AIN', 'IF710AIN', 'IF710BIN', 'IF459AIN', 'IF062AIN', 
     'IF483AIN', 'IF617AIN', 'IF656AIN', 'IF654AIN', 'IF657AIN', 'IF711AIN', 'IF711BIN']
+
 semester_value = "2025-1" # You can change the semester
-students = get_students_for_course(course_code, semester_value) #Send the semester
+students = func.get_students_for_course(course_code, semester_value) #Send the semester
 if students:
     print(f"Students for {course_code} in {semester_value}: {students}")
